d607f99cecaaa5a75264a0e5c10cf0b264f410bd
Adam Fallon <adam@adamfallon.com>
2020-12-29 00:20:02 +0000

    init commit

---
+++ monitor_mode_disable.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+while getopts i: flag
+do
+	case "${flag}" in
+		i) interface=${OPTARG};;
+	esac
+done
+
+if [ -z "$interface" ]
+then
+	exit "No interface specified!"
+else
+	echo "Enabling managed mode on interface: $interface...";
+	ifconfig $interface down;
+	iwconfig $interface mode managed;
+	ifconfig $interface up;
+	echo "Attempting to get IP address";
+	/etc/init.d/networking restart;
+fi
+
---
+++ monitor_mode_enable.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+while getopts i: flag
+do
+	case "${flag}" in
+		i) interface=${OPTARG};;
+	esac
+done
+
+if [ -z "$interface" ]
+then
+	exit "No interface specified!"
+else
+	echo "Enabling managed mode on interface: $interface...";
+	ifconfig $interface down;
+	iwconfig $interface mode monitor;
+	ifconfig $interface up;
+	echo "Monitor mode enabled";
+fi
+
